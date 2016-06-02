183da9771aca1dca29a4c43a32e8a3b49e63d951
Alkhan797 <antoine.bourguet@april.interne.fr>
2016-06-02 16:44:39 +0200

    first commit

---
+++ UnitTest1/unittest1.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "RootFinder.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Ici, votre code de test
+		}
+		TEST_METHOD(BasicTest)
+		{
+			CRootFinder rooter;
+			Assert::AreEqual(
+				// Expected value:
+				0.0,
+				// Actual value:
+				rooter.SquareRoot(0.0),
+				// Tolerance:
+				0.01,
+				// Message:
+				L"Basic test failed",
+				// Line number - used if there is no PDB file:
+				LINE_INFO());
+		}
+	};
+}
